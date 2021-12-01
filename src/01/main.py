db882e9f1029a84217b6aba0f3653f4e6e3f0c1a
Sullivan Ford <sullyjhford@gmail.com>
2021-12-01 07:46:53 +0000

    Day 1 part 2

--- src/01/main.py
+++ src/01/main.py
@@ -17,4 +17,22 @@ def calc_depth():
     print(total_greater_than_last)
 
 
+def calc_depth_in_threes():
+    lines = get_lines(__file__, 'input.txt')
+
+    total_greater_than_last = 0
+    last_line = None
+
+    for i in range(len(lines) - 2):
+        line_total = int(lines[i]) + int(lines[i + 1]) + int(lines[i + 2])
+        if not last_line:
+            last_line = line_total
+            continue
+        if int(line_total) > int(last_line):
+            total_greater_than_last += 1
+        last_line = line_total
+    print(total_greater_than_last)
+
+
 calc_depth()
+calc_depth_in_threes()
