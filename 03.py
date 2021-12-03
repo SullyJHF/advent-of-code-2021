62a4ed46f03fa32d3e7a229287dc2b329c841fc0
Sullivan Ford <sullyjhford@gmail.com>
2021-12-03 07:38:57 +0000

    Day 3

--- 03.py
+++ 03.py
@@ -5,12 +5,54 @@ from AoC import get_lines
 
 def part_1():
     lines = get_lines('./inputs/03.txt')
-    pass
+    width = len(lines[0])
+    gamma_rate = ''
+    epsilon_rate = ''
+
+    for i in range(width):
+        num_ones = 0
+        for line in lines:
+            if line[i] == '1':
+                num_ones += 1
+
+        num_zeroes = len(lines) - num_ones
+        if (num_ones > num_zeroes):
+            gamma_rate += '1'
+            epsilon_rate += '0'
+        else:
+            gamma_rate += '0'
+            epsilon_rate += '1'
+    print(int(gamma_rate, 2) * int(epsilon_rate, 2))
 
 
 def part_2():
-    lines = get_lines('./inputs/03.txt')
-    pass
+    raw_lines = get_lines('./inputs/03.txt')
+
+    def find_num(lines, i, greater):
+        if len(lines) == 1:
+            return lines[0]
+        if i >= len(lines[0]):
+            return lines[-1]
+        num_ones = 0
+        num_zeroes = 0
+        one_list = []
+        zero_list = []
+        for line in lines:
+            if line[i] == '1':
+                num_ones += 1
+                one_list.append(line)
+            else:
+                num_zeroes += 1
+                zero_list.append(line)
+        if not greater:
+            zero_list, one_list = one_list, zero_list
+        if num_ones >= num_zeroes:
+            return find_num(one_list, i + 1, greater)
+        else:
+            return find_num(zero_list, i + 1, greater)
+    oxygen = find_num(raw_lines, 0, True)
+    co2 = find_num(raw_lines, 0, False)
+    print(int(oxygen, 2) * int(co2, 2))
 
 
 if __name__ == '__main__':
