a849707515a62222f4d5aa7388c34924af896bbe
Sullivan Ford <sullyjhford@gmail.com>
2021-12-05 10:18:24 +0000

    Default day to duplicate

---
+++ default.py
@@ -0,0 +1,16 @@
+import re
+
+from AoC import get_lines
+
+
+def part_1():
+    raw_lines = get_lines('./inputs/xx-test.txt')
+
+
+def part_2():
+    raw_lines = get_lines('./inputs/xx-test.txt')
+
+
+if __name__ == '__main__':
+    part_1()
+    part_2()
